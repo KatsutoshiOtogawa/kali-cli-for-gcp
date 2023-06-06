880354434319810d5346d7afa3276b2537cae6d1
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2023-06-06 21:09:35 +0900

    update

--- 10/install.sh
+++ 10/install.sh
@@ -1,10 +1,15 @@
-#
+#!/usr/bin/env bash
 
 echo "# kali-last-snapshot is " >> /etc/apt/sources.list
 echo "deb http://http.kali.org/kali kali-last-snapshot main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://http.kali.org/kali kali-last-snapshot main contrib non-free" >> /etc/apt/sources.list
-sudo mkdir /etc/apt/preferences.d
+mkdir /etc/apt/preferences.d
 
-curl 10/etc/apt/preferences.d/kali-linux.pref
-cat < $file_path/etc/apt/preferences.d/kali-linux.pref >> /etc/apt/preferences.d/kali-linux.pref
+wget https://raw.githubusercontent.com/KatsutoshiOtogawa/kali-cli-for-gcp/main/10/etc/apt/preferences.d/kali-linux.pref
+
+mv kali-linux.pref /etc/apt/preferences.d/ 
+
 wget -qO- https://archive.kali.org/archive-key.asc | apt-key add
+
+apt update
+apt upgrade -y 
